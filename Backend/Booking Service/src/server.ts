a5abe55ef49aebe7c84895d9e940b23bd7ed5d2a
atul-1702 <atulnitish5@gmail.com>
2025-10-15 09:31:40 +0530

    feature/booking-service-db-table-added

--- Backend/Booking Service/src/server.ts
+++ Backend/Booking Service/src/server.ts
@@ -1,1 +1,8 @@
-console.log("Welcome to typescript NodeJs Project");
+import express from "express";
+import type { Express } from "express";
+
+const app: Express = express();
+
+app.listen(3000, () => {
+  console.log("Server is running at port", 3000);
+});
